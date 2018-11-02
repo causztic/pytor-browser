b6a258c7d3321d31ae10ea1fd068b3b5cf768ed5
yaojie <limyaojie93@gmail.com>
2018-11-03 00:18:23 +0800

    clean up action bar styles

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,7 +1,13 @@
-<style lang="scss" scoped>
+<style lang="scss">
+  body {
+    margin: 0;
+  }
   #navigation {
+    padding: 8px;
     width: 100%;
     display: flex;
+    align-items: center;
+    background-color: #fafafa;
     div {
       display: inline-block;
       margin-right: 15px;
@@ -10,21 +16,34 @@
       flex: 1;
       width: 100%;
       input {
+        box-sizing: border-box;
         width: inherit;
+        border: 1px solid #fff;
+        outline: none;
+        background-color: #eee;
+        padding: 5px 5px;
+        border-radius: 10px;
+        &:active, &:focus {
+          background-color: #fff;
+          border: 1px solid #aaf;
+        }
       }
+    }
+    .action-button {
+      color: #bbb;
     }
   }
 </style>
 
 <template>
   <nav id="navigation">
-    <div id="back">
+    <div id="back" class="action-button">
       <i class="fa fa-arrow-left" aria-hidden="true"></i>
     </div>
-    <div id="forward">
+    <div id="forward" class="action-button">
       <i class="fa fa-arrow-right" aria-hidden="true"></i>
     </div>
-    <div id="refresh">
+    <div id="refresh" class="action-button">
       <i class="fa fa-sync" aria-hidden="true"></i>
     </div>
     <div id="omnibox">
