18c77f5d4a775a1cbb7f89dda4dfcff7b059179d
ericywl <ericywl@live.com>
2018-11-12 10:04:30 +0800

    Refactor more_connect in client.py

--- src/common/mini_pytor/client.py
+++ src/common/mini_pytor/client.py
@@ -40,7 +40,7 @@ class Client:
         """Method to obtain items from directory"""
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # connect to directory
-        sock.connect((socket.gethostbyname(socket.gethostname()), 50000))
+        sock.connect(("localhost", 50000))
         sock.send(pickle.dumps(Cell("", ctype=CellType.GET_DIRECT)))
         received_cell = sock.recv(32768)
         received_cell = pickle.loads(received_cell)
@@ -101,6 +101,15 @@ class Client:
                 print("Something went wrong.. Signature was invalid.",
                       file=sys.stderr)
             return None
+    
+    def connect_relay(self, gonnect, gonnectport, rsa_key, connect_mode):
+        """Wrapper function for easier use"""
+        if connect_mode not in [0, 1, 2]:
+            raise Exception("Connect mode invalid.")
+        if connect_mode == 0:
+            self.first_connect(gonnect, gonnectport, rsa_key)
+        else:
+            self.more_connect(gonnect, gonnectport, rsa_key, connect_mode)
 
     def first_connect(self, gonnect, gonnectport, rsa_key):
         """Connect to the first relay of the trio"""
@@ -110,15 +119,13 @@ class Client:
             encrypted_cell, ec_privkey = Client.make_first_connect_cell(
                 rsa_key)
             if util.CLIENT_DEBUG:
-                print("First connect actual cell (decrypted bytes)")
+                print("First connect actual cell (encrypted)")
                 print(encrypted_cell)
             sock.send(encrypted_cell)  # Send them my generated ecdhe key.
             their_cell = sock.recv(4096)  # await a response.
             their_cell = pickle.loads(their_cell)
             derived_key = self.check_signature_and_derive(
-                their_cell,
-                rsa_key,
-                ec_privkey)
+                their_cell, rsa_key, ec_privkey)
             # attempt to check the signature and derive their key.
 
             if derived_key:  # ie did not return None.
@@ -133,25 +140,43 @@ class Client:
             print("Disconnected or relay is not online/ connection was "
                   + "refused.", file=sys.stderr)
 
-    def more_connect_1(self, gonnect, gonnectport, rsa_key):
-        """Connect to the next relay through the first one."""
+    def more_connect(self, gonnect, gonnectport, rsa_key, connect_mode):
+        """Connect to the next relay through my 2 connected relays."""
         encrypted_cell, ec_privkey = Client.make_first_connect_cell(rsa_key)
         if util.CLIENT_DEBUG:
             print("Innermost cell with keys (encrypted)")
             print(encrypted_cell)
 
         intermediate_relays = self.relay_list
+        # connection type. exit node always knows
         sending_cell = Cell(encrypted_cell, ctype=CellType.RELAY_CONNECT)
+        # deepest layer, encrypted with RSA
         sending_cell.ip_addr = gonnect
         sending_cell.port = gonnectport
         # inform of next port of call.
         encrypted_cell, init_vector = util.aes_encryptor(
-            intermediate_relays[0].key,
+            intermediate_relays[connect_mode - 1].key,
             sending_cell
         )
+        # encrypt using said keys.
         sending_cell = Cell(encrypted_cell, IV=init_vector,
                             ctype=CellType.RELAY_CONNECT)
-        # wrap in another cell to save init_vector
+
+        if connect_mode == 2:
+            # 2nd layer from top
+            sending_cell.ip_addr = intermediate_relays[1].ip_addr
+            sending_cell.port = intermediate_relays[1].port
+            # inform of next port of call again.
+            sending_cell = Cell(pickle.dumps(sending_cell),
+                                ctype=CellType.RELAY)
+            encrypted_cell, init_vector = util.aes_encryptor(
+                intermediate_relays[0].key,
+                sending_cell
+            )
+            sending_cell = Cell(encrypted_cell, IV=init_vector,
+                                ctype=CellType.RELAY)  # Outermost layer
+            sending_cell.ip_addr = intermediate_relays[0].ip_addr
+            sending_cell.port = intermediate_relays[0].port
 
         try:
             sock = intermediate_relays[0].sock
@@ -161,15 +186,25 @@ class Client:
                 print(pickle.dumps(sending_cell))
             their_cell = sock.recv(4096)  # await answer
             # you now receive a cell with encrypted payload.
-            their_cell = pickle.loads(their_cell)
-            decrypted = util.aes_decryptor(
-                intermediate_relays[0].key, their_cell)
             if util.CLIENT_DEBUG:
-                print(decrypted)
-            their_cell = pickle.loads(decrypted)
+                print(their_cell)
+            their_cell = pickle.loads(their_cell)
             if util.CLIENT_DEBUG:
                 print(their_cell.payload)
+            counter = 0
+            while counter < len(intermediate_relays):
+                decrypted = util.aes_decryptor(
+                    intermediate_relays[counter].key, their_cell)
+                if util.CLIENT_DEBUG:
+                    print(decrypted)
+                their_cell = pickle.loads(decrypted)
+                if util.CLIENT_DEBUG:
+                    print(their_cell.payload)
+                counter += 1
+                if counter < len(intermediate_relays):
+                    their_cell = their_cell.payload
             their_cell = pickle.loads(their_cell.payload)
+            print(their_cell.type)
 
             if their_cell.type == CellType.FAILED:
                 if util.CLIENT_DEBUG:
@@ -180,7 +215,6 @@ class Client:
 
             derived_key = self.check_signature_and_derive(
                 their_cell, rsa_key, ec_privkey)
-
             self.relay_list.append(
                 RelayData(gonnect, sock, derived_key, ec_privkey, rsa_key, gonnectport))
 
@@ -190,89 +224,11 @@ class Client:
 
         except (ConnectionResetError, ConnectionRefusedError, struct.error):
             if util.CLIENT_DEBUG:
-                print("Socket Error, removing from the list.", file=sys.stderr)
-            del self.relay_list[0]  # remove it from the lsit
-            if util.CLIENT_DEBUG:
-                print("REMOVED relay 0 DUE TO FAILED CONNECTION")
-
-    def more_connect_2(self, gonnect, gonnectport, rsa_key):
-        """Connect to the next relay through my 2 connected relays."""
-        encrypted_cell, ec_privkey = Client.make_first_connect_cell(rsa_key)
-        if util.CLIENT_DEBUG:
-            print("Innermost cell with keys (encrypted)", file=sys.stderr)
-            print(encrypted_cell)
-
-        intermediate_relays = self.relay_list
-        # connection type. exit node always knows
-        sending_cell = Cell(encrypted_cell, ctype=CellType.RELAY_CONNECT)
-        # Deepest layer, encrypted with RSA
-        sending_cell.ip_addr = gonnect
-        sending_cell.port = gonnectport
-        # inform of next port of call.
-        encrypted_cell, init_vector = util.aes_encryptor(
-            intermediate_relays[1].key,
-            sending_cell
-        )
-        # encrypt using said keys.
-        sending_cell = Cell(encrypted_cell, IV=init_vector,
-                            ctype=CellType.RELAY_CONNECT)
-        # 2nd Layer from top
-
-        sending_cell.ip_addr = intermediate_relays[1].ip_addr
-        sending_cell.port = intermediate_relays[1].port
-        # inform of next port of call again.
-        sending_cell = Cell(pickle.dumps(sending_cell), ctype=CellType.RELAY)
-
-        encrypted_cell, init_vector = util.aes_encryptor(
-            intermediate_relays[0].key,
-            sending_cell
-        )
-        sending_cell = Cell(encrypted_cell, IV=init_vector,
-                            ctype=CellType.RELAY)  # Outermost layer
-        sending_cell.ip_addr = intermediate_relays[0].ip_addr
-        sending_cell.port = intermediate_relays[0].port
-        # inform of next port of call one more time
-
-        try:
-            sock = intermediate_relays[0].sock
-            sock.send(pickle.dumps(sending_cell))  # send over the cell
-            their_cell = sock.recv(4096)  # await answer
-            # you now receive a cell with encrypted payload.
-            if util.CLIENT_DEBUG:
-                print(their_cell)
-            their_cell = pickle.loads(their_cell)
-            if util.CLIENT_DEBUG:
-                print(their_cell.payload)
-            counter = 0
-            while counter < len(intermediate_relays):
-                # print(their_cell.payload)
-                decrypted = util.aes_decryptor(
-                    intermediate_relays[counter].key, their_cell)
+                print("Socket error.", file=sys.stderr)
+            if connect_mode == 2:
+                del self.relay_list[0]  # remove it from the list
                 if util.CLIENT_DEBUG:
-                    print(decrypted)
-                their_cell = pickle.loads(decrypted)
-                # print(their_cell.payload)
-                counter += 1
-                if counter < len(intermediate_relays):
-                    their_cell = their_cell.payload
-
-            if their_cell.type == CellType.FAILED:
-                if util.CLIENT_DEBUG:
-                    print("FAILED AT CONNECTION!", file=sys.stderr)
-                return
-
-            their_cell = pickle.loads(their_cell.payload)
-            derived_key = self.check_signature_and_derive(
-                their_cell, rsa_key, ec_privkey)
-
-            self.relay_list.append(
-                RelayData(gonnect, sock, derived_key, ec_privkey, rsa_key, gonnectport))
-
-            if util.CLIENT_DEBUG:
-                print("Connected successfully to relay @ " + gonnect
-                      + "   Port: " + str(gonnectport), file=sys.stderr)
-        except struct.error:
-            print("Socket error occurred", file=sys.stderr)
+                    print("REMOVED relay 0 DUE TO FAILED CONNECTION")
 
     @staticmethod
     def req_wrapper(request, relay_list):
@@ -412,19 +368,14 @@ class Responder(BaseHTTPRequestHandler):
         # Get references from directories.
         relay_list = Client.get_directory_items()
         NUM_RELAYS = 3
-        options = {
-            0: my_client.first_connect,
-            1: my_client.more_connect_1,
-            2: my_client.more_connect_2
-        }
 
         for i in range(NUM_RELAYS):
             relay = relay_list[i]
             pubkey = serialization.load_pem_public_key(
                 relay["key"], backend=default_backend())
-            connect_func = options[i]
-            connect_func(relay["ip_addr"], relay["port"], pubkey)
-            print(my_client.relay_list)
+            my_client.connect_relay(relay["ip_addr"], relay["port"], pubkey, i)
+            if util.CLIENT_DEBUG:
+                print(my_client.relay_list)
 
         if self.path == "/favicon.ico":
             return
